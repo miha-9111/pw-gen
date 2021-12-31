3518c1c1c101b91126946774728b97255c88fc00
miha-9111 <miha-9111@yandex.ru>
2021-12-31 03:24:35 +0300

    Event Binding Syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw-gen';
+  onButtonClick() {
+    console.log('Button was clicked');
+  }
 }
